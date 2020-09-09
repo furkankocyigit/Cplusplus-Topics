f0dec10c1a7d7cfef143e8a2aeb20fd7afd84091
furkan <furkan.kocyigit@avl.com>
2020-09-09 04:53:13 -0700

    Advanced Topics course initial files are added

---
+++ Advanced_Topics/build/CMakeFiles/YourLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YourLib.dir/lib/a.cpp.o"
+  "libYourLib.a"
+  "libYourLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YourLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
