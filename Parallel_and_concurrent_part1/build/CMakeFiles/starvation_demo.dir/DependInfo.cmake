b8f1faaadb33c171c94337b9a82ab3602fa8d5e8
furkan <furkan.kocyigit@avl.com>
2020-11-02 08:53:58 -0800

    end of the parallel and concurrent programing part 1 course

---
+++ Parallel_and_concurrent_part1/build/CMakeFiles/starvation_demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/Cplusplus-Topics/Parallel_and_concurrent_part1/src/starvation_demo.cpp" "/home/ubuntu/Cplusplus-Topics/Parallel_and_concurrent_part1/build/CMakeFiles/starvation_demo.dir/src/starvation_demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  "../include"
+  "/home/ubuntu/vcpkg/installed/x64-linux/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/Cplusplus-Topics/Parallel_and_concurrent_part1/build/CMakeFiles/MyLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
